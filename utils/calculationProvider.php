36f9de46ec6a7105168cb9145b710ff4795c235b
yaarian-om <dip.kumar020@gmail.com>
2024-05-18 19:35:42 +0600

    Un-finished

--- utils/calculationProvider.php
+++ utils/calculationProvider.php
@@ -1,1 +1,11 @@
 <?php
+//
+//global $routes, $system_routes;
+//session_start();
+//
+//require '../routes.php';
+//require '../utils/system_functions.php';
+//
+//
+//require_once __DIR__ . '/../model/patientRepo.php';
+//
