7e4766a29bcf607404b8c827d1947a6a9e4d2e65
Sudipta <engineer.sudipta.kumar.das@gmail.com>
2024-05-23 22:14:41 +0600

    Updating db_connect.php to connect to the Online Database of Hostinger

--- model/db_connect.php
+++ model/db_connect.php
@@ -12,9 +12,9 @@ function db_conn()
 {
     global $error_page;
     $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "mental_health_management_system";
+    $username = "u899200555_mental_health";
+    $password = "Mental_Health#1";
+    $dbname = "u899200555_mental_health";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
